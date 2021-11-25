7eaf7af750c4023f195121d29bb38768330212da
juhyoungryu <thinkoutside2004@gmail.com>
2021-11-26 00:34:51 +0900

    업로드 페이지 작업 중

--- screens/Plus.js
+++ screens/Plus.js
@@ -1,26 +1,59 @@
-import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/core";
 import {
+  KeyboardAvoidingView,
   StyleSheet,
   Text,
   TextInput,
   View,
   TouchableOpacity,
+  BackHandler,
+  Alert,
 } from "react-native";
 
 const Plus = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const backAction = () => {
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.input} placeholder="title"></TextInput>
-      <TextInput style={styles.input} placeholder="price"></TextInput>
-      <TextInput
-        style={styles.inputCon}
-        placeholder="content"
-        maxLength={100}
-        multiline={true}
-      ></TextInput>
-      <TouchableOpacity>
-        <Text style={styles.pushBtn}>올리기</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 3 }}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="title"></TextInput>
+        <TextInput style={styles.input} placeholder="price"></TextInput>
+        <TextInput
+          style={styles.inputCon}
+          placeholder="content"
+          maxLength={100}
+          multiline={true}
+        ></TextInput>
+        <TouchableOpacity style={styles.pushBtn}>
+          <Text style={styles.pushBtnText}>올리기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnCon}>
+          <AntDesign
+            name="close"
+            size={24}
+            color="#000"
+            style={styles.btn}
+            onPress={() => {
+              navigation.replace("Home");
+            }}
+          ></AntDesign>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -29,13 +62,14 @@ export default Plus;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 100,
   },
   input: {
     width: "80%",
-    margin: 5,
+    margin: 10,
     padding: 7,
     borderWidth: 1,
     borderRadius: 5,
@@ -52,10 +86,22 @@ const styles = StyleSheet.create({
   pushBtn: {
     backgroundColor: "#dc3545",
     borderRadius: 5,
-    padding: 10,
-    width: "15%",
+    padding: 12,
+    width: "20%",
+    marginTop: 10,
+  },
+  pushBtnText: {
     textAlign: "center",
     color: "white",
-    marginTop: 20,
+    fontSize: 14,
+  },
+  btnCon: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  btn: {
+    fontSize: 30,
+    marginBottom: 40,
   },
 });

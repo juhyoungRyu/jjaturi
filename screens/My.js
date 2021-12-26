7a2020118dcb13a7af6a52439f9fd0d03566aae1
juhyoungryu <thinkoutside2004@gmail.com>
2021-12-27 01:44:38 +0900

    게시글 상세 ui 구현 완료, 프로필 구현 중

--- screens/My.js
+++ screens/My.js
@@ -1,78 +1,160 @@
 import { useNavigation } from "@react-navigation/core";
 import { AntDesign } from "@expo/vector-icons";
 import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 import { auth } from "../firebase";
 import LoginScreen from "./LoginScreen";
 
 const My = () => {
   const navigation = useNavigation();
 
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("Login");
-        console.log("sign out");
-      })
-      .catch((error) => alert(error.message));
-  };
+  // const handleSignOut = () => {
+  //   auth
+  //     .signOut()
+  //     .then(() => {
+  //       navigation.replace("Login");
+  //       console.log("sign out");
+  //     })
+  //     .catch((error) => alert(error.message));
+  // };
 
   return (
-    <View style={styles.container1}>
-      <View style={styles.title}>
-        <Text style={styles.mypage}>마이페이지</Text>
-        <TouchableOpacity>
-          <AntDesign
-            name="setting"
-            size={28}
-            color="black"
-            style={{ marginRight: 20 }}
-          />
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          flex: 0.3,
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <View>
-          <View style={styles.emptyImage} />
-        </View>
-        <View style={{ flexDirection: "column", marginLeft: 10 }}>
-          <View style={{ flex: 0.6, marginBottom: 5 }}>
-            <Text style={styles.name}>User name</Text>
-            <Text style={styles.tag}>#000000</Text>
+    <View style={styles.container}>
+      <View style={styles.one}>
+        <View style={styles.top}>
+          <View style={styles.topNav}>
+            <Text style={{ marginLeft: 30, fontSize: 22 }}>마이페이지</Text>
+            <TouchableOpacity>
+              <AntDesign
+                name="setting"
+                size={27}
+                color="black"
+                style={{ marginRight: 30 }}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={{ flexDirection: "row" }}>
-            <AntDesign name="checkcircle" size={17} color="grey" />
-            <Text style={{ marginLeft: 7 }}>채팅링크 등록 완료</Text>
+          <View style={styles.profile}>
+            <View style={styles.photo} />
+            <View
+              style={{
+                flexDirection: "column",
+                marginTop: 0,
+              }}
+            >
+              <View>
+                <Text style={{ fontSize: 18, color: "black" }}>User Name</Text>
+                <Text style={{ fontSize: 13, color: "#444" }}>#000000</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 15,
+                  alignItems: "center",
+                }}
+              >
+                <AntDesign name="checkcircle" size={13} color="#888" />
+                <Text style={{ fontSize: 12, color: "#444", marginLeft: 5 }}>
+                  채팅링크 등록 완료
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity style={{ width: "100%" }}>
+              <Text style={styles.editBtn}>정보 수정</Text>
+            </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.editBtn}></View>
       </View>
-      <View style={styles.container2}>
-        {/* <Text style={styles.email}>Email : {auth.currentUser?.email}</Text> */}
-        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-          <Text style={styles.buttonText}>Sign Out</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.container3}>
-        <TouchableOpacity>
-          <AntDesign
-            name="close"
-            size={24}
-            color="#000"
-            style={styles.btn}
-            onPress={() => {
-              navigation.replace("Home");
+      <View style={styles.two}>
+        <View style={styles.buttonCon}>
+          <TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "row",
+                backgroundColor: "rgb(72,156,203)",
+                padding: 15,
+                borderRadius: 30,
+              }}
+            >
+              <FontAwesome name="shopping-basket" size={24} color="white" />
+              <Text style={{ color: "white", marginLeft: 13, fontSize: 18 }}>
+                판매 목록
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "row",
+                backgroundColor: "rgb(72,156,203)",
+                padding: 15,
+                borderRadius: 30,
+              }}
+            >
+              <Entypo name="heart" size={24} color="white" />
+              <Text style={{ color: "white", marginLeft: 13, fontSize: 18 }}>
+                관심 목록
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.manual}>
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "center",
             }}
-          ></AntDesign>
-        </TouchableOpacity>
+          >
+            <Ionicons
+              name="document-text-outline"
+              size={29}
+              color="#444"
+              style={{ marginTop: 20 }}
+            />
+            <Text
+              style={{
+                fontSize: 22,
+                color: "#444",
+                marginLeft: 10,
+                marginTop: 20,
+              }}
+            >
+              앱 설명서
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.setting}>
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Ionicons
+              name="settings"
+              size={29}
+              color="#444"
+              style={{ marginTop: 20 }}
+            />
+            <Text
+              style={{
+                fontSize: 22,
+                color: "#333",
+                marginLeft: 10,
+                marginTop: 20,
+                marginBottom: 20,
+              }}
+            >
+              앱 설정
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      <View style={styles.three}></View>
     </View>
   );
 };
@@ -80,61 +162,67 @@ const My = () => {
 export default My;
 
 const styles = StyleSheet.create({
-  container1: {
+  container: {
     flex: 1,
   },
-  container2: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+  one: {
+    flex: 0.22,
+    borderBottomWidth: 1,
+    borderColor: "#444",
   },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    alignItems: "center",
-    borderRadius: 15,
-    marginTop: 40,
+  two: {
+    flex: 0.7,
   },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
+  three: {
+    flex: 0.12,
+    backgroundColor: "blue",
   },
-  email: {
-    textAlign: "center",
-    width: "80%",
-    marginTop: 50,
-    fontSize: 17,
-    fontWeight: "300",
-    borderRadius: 15,
-  },
-  container3: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "center",
-  },
-  btn: {
-    fontSize: 30,
-    marginBottom: 40,
-  },
-  title: {
-    flex: 0.2,
+  topNav: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginTop: 20,
+    marginTop: 30,
   },
-  mypage: {
-    fontSize: 23,
-    marginLeft: 20,
+  profile: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 25,
+    alignItems: "flex-start",
   },
-  emptyImage: {
-    backgroundColor: "grey",
-    padding: 55,
-    borderRadius: 80,
-    marginLeft: 20,
+  photo: {
+    padding: 40,
+    backgroundColor: "#888",
+    width: "10%",
+    borderRadius: 60,
+    marginLeft: 30,
+    marginRight: 10,
+  },
+  editBtn: {
+    padding: 10,
+    backgroundColor: "rgb(72,156,203)",
+    borderRadius: 30,
+    fontSize: 18,
+    color: "white",
+    width: "22%",
+    textAlign: "center",
+    marginTop: 15,
+    marginLeft: 55,
+  },
+  buttonCon: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  manual: {
+    borderTopWidth: 1,
+    marginBottom: 25,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  setting: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
   },
 });

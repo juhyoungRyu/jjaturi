de00e996cf0328e255a9b2948e492ac712db6afa
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-08 17:44:33 +0900

    여러가지 수정

--- screens/CategoryScreen2.js
+++ screens/CategoryScreen2.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 const Stack = createNativeStackNavigator();
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { firestore } from "../firebase";
@@ -42,7 +42,12 @@ const HomeScreen = ({ navigation }) => {
               navigation.navigate("buy", { src: master });
             }}
           >
-            <View style={styles.icon} />
+            <Image
+              style={styles.icon}
+              source={{
+                uri: "https://firebasestorage.googleapis.com/v0/b/jjaturi-d75ad.appspot.com/o/default%2Fme.png?alt=media&token=8aeeb9a2-11de-4d9d-8470-a2c741391067",
+              }}
+            />
             <Text>삽니다</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -51,7 +56,12 @@ const HomeScreen = ({ navigation }) => {
               navigation.navigate("paint", { src: master });
             }}
           >
-            <View style={styles.icon} />
+            <Image
+              style={styles.icon}
+              source={{
+                uri: "https://firebasestorage.googleapis.com/v0/b/jjaturi-d75ad.appspot.com/o/default%2Fpaint.png?alt=media&token=0d4d037d-3de7-4ad9-b070-2b99e8552f94",
+              }}
+            />
             <Text>페인트</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -60,7 +70,12 @@ const HomeScreen = ({ navigation }) => {
               navigation.navigate("tools", { src: master });
             }}
           >
-            <View style={styles.icon} />
+            <Image
+              style={styles.icon}
+              source={{
+                uri: "https://firebasestorage.googleapis.com/v0/b/jjaturi-d75ad.appspot.com/o/default%2Ftool1.png?alt=media&token=0dbc307c-add2-4662-b98e-f4d09a1a4b05",
+              }}
+            />
             <Text>공구</Text>
           </TouchableOpacity>
         </View>
@@ -71,7 +86,12 @@ const HomeScreen = ({ navigation }) => {
               navigation.navigate("wallpaper", { src: master });
             }}
           >
-            <View style={styles.icon} />
+            <Image
+              style={styles.icon}
+              source={{
+                uri: "https://firebasestorage.googleapis.com/v0/b/jjaturi-d75ad.appspot.com/o/default%2Fwallpaper.png?alt=media&token=0a7c5bbf-f2c3-4752-8e8b-8e90ee476149",
+              }}
+            />
             <Text>벽지</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -80,7 +100,7 @@ const HomeScreen = ({ navigation }) => {
               alert("준비중입니다!");
             }}
           >
-            <View style={styles.icon} />
+            <View style={styles.cms} />
             <Text>Coming Soon!</Text>
           </TouchableOpacity>
         </View>
@@ -127,7 +147,11 @@ const Paint = ({ route, navigation }) => {
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View>
         {dataRe2.map((obj, key) => (
-          <Text key={key}>{obj.name}</Text>
+          <View key={key} style={styles.paintCon}>
+            <Text>{obj.name}</Text>
+            <Text>{obj.price}</Text>
+            <Text>{obj.gps}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -228,14 +252,22 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   icon: {
-    padding: 40,
-    backgroundColor: "grey",
-    width: "20%",
+    height: 100,
+    width: 100,
     marginBottom: 5,
-    borderRadius: 50,
+    borderRadius: 60,
+    resizeMode: "contain",
+  },
+  cms: {
+    height: 100,
+    width: 100,
+    marginBottom: 5,
+    backgroundColor: "grey",
+    borderRadius: 100,
   },
   con: {
     alignItems: "center",
     width: "30%",
   },
+  paintCon: {},
 });
